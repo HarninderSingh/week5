040fbb38ddeabfdc477380cceb70c752bf8cecb9
HarninderSingh <singhharninder51199@gmail.com>
2024-10-04 17:18:14 -0400

    Added struct cars

--- main.go
+++ main.go
@@ -1,5 +1,13 @@
 package main
 
+// Task struct defines the task structure
+type Task struct {
+	ID          int    `json:"id"`
+	Title       string `json:"title"`
+	Description string `json:"description"`
+	Status      string `json:"status"`
+}
+
 func main() {
 
 }
